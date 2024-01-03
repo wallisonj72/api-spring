77e17f29e4443d2a4ae3b9e924ddb795a0dac1c6
Wallison <wallisonj72@gmail.com>
2024-01-03 11:51:08 -0300

    metodos delete() e edit()

--- src/main/java/com/wallison/api/controller/StudentController.java
+++ src/main/java/com/wallison/api/controller/StudentController.java
@@ -2,7 +2,9 @@ package com.wallison.api.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +25,15 @@ public record StudentController(StudentService studentService) {
     @GetMapping
     public List<Student> toList() {
         return studentService.toList();
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        studentService.delete(id);
+    }
+
+    @GetMapping("/{id}")
+    public Student edit(@PathVariable Long id) {
+        return studentService.edit(id);
     }
 }
--- src/main/java/com/wallison/api/service/StudentService.java
+++ src/main/java/com/wallison/api/service/StudentService.java
@@ -2,6 +2,7 @@ package com.wallison.api.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wallison.api.model.Student;
@@ -9,11 +10,20 @@ import com.wallison.api.repository.StudentRepository;
 
 @Service
 public record StudentService(StudentRepository studentRepository) {
+
     public Student save(Student student) {
         return studentRepository.save(student);
     }
 
     public List<Student> toList() {
         return studentRepository.findAll();
+    }
+
+    public void delete(Long id) {
+        studentRepository.deleteById(id);
+    }
+
+    public Student edit(Long id) {
+        return studentRepository.findById(id).orElse(null);
     }
 }
